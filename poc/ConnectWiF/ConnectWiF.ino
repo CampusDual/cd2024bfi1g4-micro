08f5424316359e167832c2f15fa5a74991e7d7e4
Kurosaki <pablobasantadominguez@gmail.com>
2024-12-02 11:35:09 +0000

    pocWifiTempHum

---
+++ poc/ConnectWiF/ConnectWiF.ino
@@ -0,0 +1,33 @@
+#include <WiFi.h>
+
+const char* ssid = "";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println("\nConectando al Wi-Fi...");
+  WiFi.begin(ssid, password);
+
+  int max_retries = 20; 
+  while (WiFi.status() != WL_CONNECTED && max_retries > 0) {
+    delay(1000);
+    Serial.print(".");
+    max_retries--;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\n¡Conexión exitosa!");
+    Serial.print("Dirección IP: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nNo se pudo conectar al Wi-Fi.");
+    Serial.println("Revisa el nombre y la contraseña.");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
